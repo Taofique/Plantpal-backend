fbcfbe7ac7f1f038793e91365ca83e96e06237ab
Taofique <shornob8@gmail.com>
2025-10-01 13:59:57 +0600

    fix: fixed the db.ts and server.ts to establish connection and fix the issues in render deploy

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,10 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
-dotenv.config();
+// Only load .env file in development
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   dialect: "postgres",
